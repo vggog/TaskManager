0ece4762a25ea8ab38eea3ebd5babb59d1508ef3
Айдар <gluhovkich@gmail.com>
2024-05-14 17:16:35 +0300

    Добавить контроллер для задач и метод гет для получения всех задач

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using TaskManager.Models;
+using TaskManager.Repositories;
+
+namespace TaskManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        /// <summary>
+        /// Получить все задачи.
+        /// </summary>
+        [HttpGet]
+        [ProducesResponseType(typeof(TaskModel), (int)HttpStatusCode.OK)]
+        public IEnumerable<TaskModel> Get()
+        {
+            TaskRepository taskRepository = new();
+            return taskRepository.GetAll();
+        }
+    }
+}
